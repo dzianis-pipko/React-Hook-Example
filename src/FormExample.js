8540fabe43ca61ea77016d43952edf7fb92b9b81
Denis Pipko <denis.pipko@kpigroups.com>
2022-03-31 16:04:20 +0300

    useState "Refactoring formExample component"

--- src/FormExample.js
+++ src/FormExample.js
@@ -1,230 +1,218 @@
 // Стейт в отдельных обьектах
 
-// import React, {useState} from "react";
+import React, {useState} from "react";
 
-// const FormInput = (props) => {
-//     const {name, label, value, onChange, type = 'text'} = props
-//     return(
-//         <div>
-//             <label htmlFor={name}>{label}</label>
-//             <input name={name} type={type} value={value} onChange={e => onChange(e.target.value)} />
-//         </div>
-//     )
-// }
+const FormInput1 = (props) => {
+    const {name, label, value, onChange, type = 'text'} = props
+    return(
+        <div>
+            <label htmlFor={name}>{label}</label>
+            <input name={name} type={type} value={value} onChange={e => onChange(e.target.value)} />
+        </div>
+    )
+}
 
-// const DEFAULT_VALUE = 21;
+const DEFAULT_VALUE_1 = 21;
 
-// export const FormExample = () => {
+export const FormExample1 = () => {
 
-//     const [firstName, setFirstName] = useState('');
-//     const [lastName, setLastName] = useState('');
-//     const [age, setAge] = useState(DEFAULT_VALUE);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [age, setAge] = useState(DEFAULT_VALUE_1);
 
-//     const clear = () => {
-//         setFirstName('');
-//         setLastName('');
-//         setAge(DEFAULT_VALUE);
-//     }
+    const clear = () => {
+        setFirstName('');
+        setLastName('');
+        setAge(DEFAULT_VALUE_1);
+    }
 
-//     return (
-//         <>
-//             <form>
-//                 <FormInput
-//                     name='firstName'
-//                     label='first name'
-//                     value={firstName}
-//                     onChange={newValue => setFirstName(newValue)}
-//                 />
-//                 <FormInput
-//                     name='lastName'
-//                     label='last name'
-//                     value={lastName}
-//                     onChange={newValue => setLastName(newValue)}
-//                 />
-//                 <FormInput
-//                     name='age'
-//                     label='age'
-//                     value={age}
-//                     onChange={newValue => setAge(newValue ? parseInt(newValue) : 0)}
-//                     type='number'
-//                 />
-//             </form>
-//             <div>
-//                 <button onClick={clear}>clear</button>
-//             </div>
-//             <div>
-//                 first Name: {firstName}; <br/>
-//                 last Name: {lastName}; <br/> 
-//                 age: {age};
-//             </div>
-//         </>
-//     )
-// }
+    return (
+        <>
+            <form>
+                <FormInput1
+                    name='firstName'
+                    label='first name'
+                    value={firstName}
+                    onChange={newValue => setFirstName(newValue)}
+                />
+                <FormInput1
+                    name='lastName'
+                    label='last name'
+                    value={lastName}
+                    onChange={newValue => setLastName(newValue)}
+                />
+                <FormInput1
+                    name='age'
+                    label='age'
+                    value={age}
+                    onChange={newValue => setAge(newValue ? parseInt(newValue) : 0)}
+                    type='number'
+                />
+            </form>
+            <div>
+                <button onClick={clear}>clear</button>
+            </div>
+            <div>
+                first Name: {firstName}; <br/>
+                last Name: {lastName}; <br/> 
+                age: {age};
+            </div>
+        </>
+    )
+}
 
 // =========================================================================
-
 // Классовый стейт
 
-// import React from "react";
+const FormInput2 = (props) => {
+    const {name, label, value, onChange, type = 'text'} = props
+    return(
+        <div>
+            <label htmlFor={name}>{label}</label>
+            <input name={name} type={type} value={value} onChange={e => onChange(e.target.value)} />
+        </div>
+    )
+}
 
-// const FormInput = (props) => {
-//     const {name, label, value, onChange, type = 'text'} = props
-//     return(
-//         <div>
-//             <label htmlFor={name}>{label}</label>
-//             <input name={name} type={type} value={value} onChange={e => onChange(e.target.value)} />
-//         </div>
-//     )
-// }
+const DEFAULT_VALUE_2 = 21;
 
-// const DEFAULT_VALUE = 21;
+const initialState = {
+    firstName: '',
+    lastName: '',
+    age: DEFAULT_VALUE_2
+}
 
-// const initialState = {
-//     firstName: '',
-//     lastName: '',
-//     age: DEFAULT_VALUE
-// }
+export class FormExample2 extends React.Component {
 
-// export class FormExample extends React.Component {
-
-//     constructor(props){
-//         super(props);
-//         this.state = initialState;
-//     }
+    constructor(props){
+        super(props);
+        this.state = initialState;
+    }
 
 
-//     clearState = () => {
-//         this.setState(initialState);
-//     }
+    clearState = () => {
+        this.setState(initialState);
+    }
 
-//     render() {
-//         return (
-//             <>
-//                 <form>
-//                     <FormInput
-//                         name='firstName'
-//                         label='first name'
-//                         value={this.state.firstName}
-//                         onChange={firstName => this.setState({firstName})}
-//                     />
-//                     <FormInput
-//                         name='lastName'
-//                         label='last name'
-//                         value={this.state.lastName}
-//                         onChange={lastName => this.setState({lastName})}
-//                     />
-//                     <FormInput
-//                         name='age'
-//                         label='age'
-//                         value={this.state.age}
-//                         onChange={ageString => {
-//                             const age = ageString ? parseInt(ageString) : 0
-//                             this.setState({age});
-//                         }}
-//                         type='number'
-//                     />
-//                 </form>
-//                 <div>
-//                     <button onClick={this.clearState}>clear</button>
-//                 </div>
-//                 <div>
-//                     first Name: {this.state.firstName}; <br/>
-//                     last Name: {this.state.lastName}; <br/> 
-//                     age: {this.state.age};
-//                 </div>
-//             </>
-//         )
-//     }
-// }
+    render() {
+        return (
+            <>
+                <form>
+                    <FormInput2
+                        name='firstName'
+                        label='first name'
+                        value={this.state.firstName}
+                        onChange={firstName => this.setState({firstName})}
+                    />
+                    <FormInput2
+                        name='lastName'
+                        label='last name'
+                        value={this.state.lastName}
+                        onChange={lastName => this.setState({lastName})}
+                    />
+                    <FormInput2
+                        name='age'
+                        label='age'
+                        value={this.state.age}
+                        onChange={ageString => {
+                            const age = ageString ? parseInt(ageString) : 0
+                            this.setState({age});
+                        }}
+                        type='number'
+                    />
+                </form>
+                <div>
+                    <button onClick={this.clearState}>clear</button>
+                </div>
+                <div>
+                    first Name: {this.state.firstName}; <br/>
+                    last Name: {this.state.lastName}; <br/> 
+                    age: {this.state.age};
+                </div>
+            </>
+        )
+    }
+}
 
 // =========================================
-
 // Стейт в одном обьекте
 
-// import React, {useState} from "react";
+const FormInput3 = (props) => {
+    const {name, label, value, onChange, type = 'text'} = props
+    return(
+        <div>
+            <label htmlFor={name}>{label}</label>
+            <input name={name} type={type} value={value} onChange={e => onChange(e.target.value)} />
+        </div>
+    )
+}
 
-// const FormInput = (props) => {
-//     const {name, label, value, onChange, type = 'text'} = props
-//     return(
-//         <div>
-//             <label htmlFor={name}>{label}</label>
-//             <input name={name} type={type} value={value} onChange={e => onChange(e.target.value)} />
-//         </div>
-//     )
-// }
+const DEFAULT_VALUE_3 = 21;
 
-// const DEFAULT_VALUE = 21;
+const initialState3 = {
+    firstName: '',
+    lastName: '',
+    age: DEFAULT_VALUE_3
+}
 
-// const initialState = {
-//     firstName: '',
-//     lastName: '',
-//     age: DEFAULT_VALUE
-// }
+export const FormExample3 = () => {
 
-// export const FormExample = () => {
+    const [state, setState] = useState(initialState3);
 
-//     const [state, setState] = useState(initialState);
+    const clear = () => {
+        setState(initialState3);
+    }
 
-//     const clear = () => {
-//         setState(initialState);
-//     }
-
-//     return (
-//         <>
-//             <form>
-//                 <FormInput
-//                     name='firstName'
-//                     label='first name'
-//                     value={state.firstName}
-//                     onChange={newValue => setState(prewValue => {
-//                         return {
-//                             ...prewValue,
-//                             firstName: newValue,
-//                         }
-//                     })}
-//                 />
-//                 <FormInput
-//                     name='lastName'
-//                     label='last name'
-//                     value={state.lastName}
-//                     onChange={newValue => setState(prewValue => {
-//                         return {
-//                             ...prewValue,
-//                             lastName: newValue,
-//                         }
-//                     })}
-//                 />
-//                 <FormInput
-//                     name='age'
-//                     label='age'
-//                     value={state.age}
-//                     onChange={newValue => setState(prewValue => {
-//                         return {
-//                             ...prewValue,
-//                             age: newValue ? parseInt(newValue) : 0,
-//                         }
-//                     })}
-//                     type='number'
-//                 />
-//             </form>
-//             <div>
-//                 <button onClick={clear}>clear</button>
-//             </div>
-//             <div>
-//                 {/* first Name: {state.firstName}; <br/>
-//                 last Name: {state.lastName}; <br/> 
-//                 age: {state.age}; */}
-//                 {JSON.stringify(state)}
-//             </div>
-//         </>
-//     )
-// }
+    return (
+        <>
+            <form>
+                <FormInput3
+                    name='firstName'
+                    label='first name'
+                    value={state.firstName}
+                    onChange={newValue => setState(prewValue => {
+                        return {
+                            ...prewValue,
+                            firstName: newValue,
+                        }
+                    })}
+                />
+                <FormInput3
+                    name='lastName'
+                    label='last name'
+                    value={state.lastName}
+                    onChange={newValue => setState(prewValue => {
+                        return {
+                            ...prewValue,
+                            lastName: newValue,
+                        }
+                    })}
+                />
+                <FormInput3
+                    name='age'
+                    label='age'
+                    value={state.age}
+                    onChange={newValue => setState(prewValue => {
+                        return {
+                            ...prewValue,
+                            age: newValue ? parseInt(newValue) : 0,
+                        }
+                    })}
+                    type='number'
+                />
+            </form>
+            <div>
+                <button onClick={clear}>clear</button>
+            </div>
+            <div>
+                {JSON.stringify(state)}
+            </div>
+        </>
+    )
+}
 
 // ==================================
-
 // Стейт в одном обьекте c кастомным хуком.
-
-import React, {useState} from "react";
 
 const useMeargeValue = (initialValue) => {
 
@@ -240,7 +228,7 @@ const useMeargeValue = (initialValue) => {
     return [state, mergeValue];
 }
 
-const FormInput = (props) => {
+const FormInput4 = (props) => {
     const {name, label, value, onChange, type = 'text'} = props
     return(
         <div>
@@ -250,38 +238,38 @@ const FormInput = (props) => {
     )
 }
 
-const DEFAULT_VALUE = 21;
+const DEFAULT_VALUE_4 = 21;
 
-const initialState = {
+const initialState4 = {
     firstName: '',
     lastName: '',
-    age: DEFAULT_VALUE
+    age: DEFAULT_VALUE_4
 }
 
-export const FormExample = () => {
+export const FormExample4 = () => {
 
-    const [state, setState] = useMeargeValue(initialState);
+    const [state, setState] = useMeargeValue(initialState4);
 
     const clear = () => {
-        setState(initialState);
+        setState(initialState4);
     }
 
     return (
         <>
             <form>
-                <FormInput
+                <FormInput4
                     name='firstName'
                     label='first name'
                     value={state.firstName}
                     onChange={firstName => setState({firstName})}
                 />
-                <FormInput
+                <FormInput4
                     name='lastName'
                     label='last name'
                     value={state.lastName}
                     onChange={lastName => setState({lastName})}
                 />
-                <FormInput
+                <FormInput4
                     name='age'
                     label='age'
                     value={state.age}
@@ -293,9 +281,6 @@ export const FormExample = () => {
                 <button onClick={clear}>clear</button>
             </div>
             <div>
-                {/* first Name: {state.firstName}; <br/>
-                last Name: {state.lastName}; <br/> 
-                age: {state.age}; */}
                 {JSON.stringify(state)}
             </div>
         </>
